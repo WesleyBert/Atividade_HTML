8dd27c68391d7f1095fa4312d14690db5e629f4c
wesley berto <wesleyberto4@gmail.com>
2024-01-12 15:15:13 -0300

    Adicionando javascript para o projeto

--- script/script.js
+++ script/script.js
@@ -1,10 +1,53 @@
-// function of Hello worl end js
+
+let nome = document.getElementById('nome');
+let email = document.getElementById('email');
+let assunto = document.querySelector('#assunto')
+let nomeOk = false
+let emailOk = false
+let assuntoOk = false
+// let assunto = document.querySelect('#assunto');
+
+function validaNome() {
+    let txt = document.querySelector('#nomeTxt')
+    if (nome.value.length < 3) {
+        txt.innerHTML = "Nome Inválido"
+        txt.style.color = 'red'
+
+    } else {
+        txt.innerHTML = ""
+        nomeOk = true
+    }
+}
+function validaEmail() {
+    let txtEmail = document.querySelector('#emailTxt')
+    if (email.value.indexOf('@') == -1 || email.value.indexOf('.') == -1) {
+        txtEmail.innerHTML = "Email Inválido"
+        txtEmail.style.color = 'red'
+        txtEmail.style.display = 'block'
+    } else {
+        txtEmail.innerHTML = ""
+        txtEmail.style.display = 'block'
+        emailOk = true
+    }
+}
+
+function validaAssunto() {
+    let txtAssunto = document.querySelector('#assuntoTxt')
+    if (assunto.value.length >= 100) {
+        txtAssunto.innerHTML = "<p>Texto muito grande, no maximo 100 caracteres</p>"
+        txtAssunto.style.color = 'red'
+        txtAssunto.style.display = 'block'
+    } else {
+        txtAssunto.style.display = 'none'
+        assuntoOk = true
+    }
+}
 
 
-
-nome = document.getElementById('nome');
-
-
-function helloWorld() {
-    alert(nome.value)
+function validarEnviar() {
+    if (nomeOk === true && emailOk === true && assuntoOk === true) {
+        alert("Formulario enviado com sucesso!")
+    } else {
+        alert("Preencha o formulario corretamente!")
+    }
 }
